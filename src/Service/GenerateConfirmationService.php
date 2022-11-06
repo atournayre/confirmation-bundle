b7e3309ecd7f1c9ec58a11307e010d140c03b224
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2022-11-06 21:12:05 +0100

    Update GenerateConfirmationService : use AbstractProvider.

--- src/Service/GenerateConfirmationService.php
+++ src/Service/GenerateConfirmationService.php
@@ -2,25 +2,36 @@
 
 namespace Atournayre\Bundle\ConfirmationBundle\Service;
 
+use Atournayre\Bundle\ConfirmationBundle\Config\LoaderConfig;
 use Atournayre\Bundle\ConfirmationBundle\Contracts\ConfirmableInterface;
+use Atournayre\Bundle\ConfirmationBundle\DTO\ConfirmationCodeDTO;
+use Atournayre\Bundle\ConfirmationBundle\Entity\ConfirmationCode;
 use Atournayre\Bundle\ConfirmationBundle\Exception\ConfirmationCodeException;
 use Atournayre\Bundle\ConfirmationBundle\Factory\ConfirmationCodeFactory;
+use Atournayre\Bundle\ConfirmationBundle\Provider\AbstractProvider;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Exception\ORMException;
 use Exception;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 use ReflectionClass;
-use ReflectionException;
-use stdClass;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
 use Symfony\Component\Uid\Uuid;
 
 class GenerateConfirmationService
 {
-    // TODO Remplacer par un paramètre de configuration ?
-    private const  DEFAULT_NUMBERS_OF_DIGITS_FOR_CODE = 6;
+    private const DEFAULT_NUMBERS_OF_DIGITS_FOR_CODE = 6;
+
+    /**
+     * @var AbstractProvider
+     */
+    private $provider;
 
     public function __construct(
         private readonly EntityManagerInterface $entityManager,
-        private readonly NotifierService        $notifierService,
+        private readonly LoaderConfig           $loaderConfig,
+        private readonly ContainerInterface     $container,
     ) {
     }
 
@@ -30,11 +41,14 @@ class GenerateConfirmationService
     public function __invoke(Uuid $targetId, string $type): void
     {
         try {
-            $this->createConfirmationCode($type, $targetId);
-            $this->updateTargetedEntity($type, $targetId);
+            $confirmationCodeDTO = new ConfirmationCodeDTO($targetId, $type);
+            $this->provider = $this->getProvider($confirmationCodeDTO);
+
+            $confirmationCode = $this->createConfirmationCode($type, $targetId);
+            $this->updateTargetedEntity($confirmationCode);
             $this->entityManager->flush();
-            ($this->notifierService)($targetId, $type);
-        } catch (Exception $exception) {
+            $this->provider->notify($confirmationCode);
+        } catch (NotFoundExceptionInterface|ContainerExceptionInterface|Exception $exception) {
             throw ConfirmationCodeException::fromException($exception);
         }
     }
@@ -42,42 +56,53 @@ class GenerateConfirmationService
     /**
      * @throws Exception
      */
-    private function createConfirmationCode(string $type, Uuid $targetId): void
+    private function createConfirmationCode(string $type, Uuid $targetId): ConfirmationCode
     {
         $confirmationCode = ConfirmationCodeFactory::create($type, $targetId, self::DEFAULT_NUMBERS_OF_DIGITS_FOR_CODE);
         $this->entityManager->persist($confirmationCode);
+        return $confirmationCode;
     }
 
     /**
-     * @param string $type
-     * @param Uuid   $targetId
-     *
-     * @return void
      * @throws ConfirmationCodeException
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
      * @throws ORMException
-     * @throws ReflectionException
      */
-    private function updateTargetedEntity(string $type, Uuid $targetId): void
+    private function updateTargetedEntity(ConfirmationCode $confirmationCode): void
     {
-        $entityName = $this->getEntityClassFromMapping($type);
-
-        $reflectionClass = new ReflectionClass($entityName);
-        $implementsInterface = $reflectionClass->implementsInterface(ConfirmableInterface::class);
-
-        if (!$implementsInterface) {
-            throw ConfirmationCodeException::mustImplementInterface($entityName);
-        }
-
-        $entity = $this->entityManager->getReference($entityName, $targetId);
-
+        $targetId = $confirmationCode->getTargetId();
+        $entity = $this->provider->getEntity($targetId);
+        $this->verifyIfEntityImplementsConfirmableInterface($entity);
+        $entity = $this->entityManager->getReference(get_class($entity), $targetId);
         $entity->setAsNotConfirmed();
-
         $this->entityManager->persist($entity);
     }
 
-    private function getEntityClassFromMapping(string $type): string
+    /**
+     * @throws ConfirmationCodeException
+     */
+    private function verifyIfEntityImplementsConfirmableInterface(object $entity)
     {
-        // TODO Recuperer le entity_class associé au mapping passé en paramètre
-        return stdClass::class;
+        $reflectionClass = new ReflectionClass($entity);
+        $implementsInterface = $reflectionClass->implementsInterface(ConfirmableInterface::class);
+
+        if ($implementsInterface) return;
+
+        throw ConfirmationCodeException::mustImplementInterface($entity);
+    }
+
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     * @throws ConfirmationCodeException
+     */
+    private function getProvider(ConfirmationCodeDTO $confirmationCodeDTO): AbstractProvider
+    {
+        $providerClassName = $this->loaderConfig->getProvider($confirmationCodeDTO->type);
+
+        if ($this->container->has($providerClassName)) return $this->container->get($providerClassName);
+
+        throw new ServiceNotFoundException($providerClassName);
     }
 }
